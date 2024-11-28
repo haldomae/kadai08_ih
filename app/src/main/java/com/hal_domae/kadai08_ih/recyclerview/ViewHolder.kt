58c596876e4287d7df0e8f305e37c9f76541a912
haldomae <hal.domae@gmail.com>
2024-11-28 17:37:32 +0900

    Holderの中身を記述

--- app/src/main/java/com/hal_domae/kadai08_ih/recyclerview/ViewHolder.kt
+++ app/src/main/java/com/hal_domae/kadai08_ih/recyclerview/ViewHolder.kt
@@ -1,4 +1,11 @@
 package com.hal_domae.kadai08_ih.recyclerview
 
-class ViewHolder {
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.hal_domae.kadai08_ih.R
+
+class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    val itemDate: TextView = itemView.findViewById(R.id.date)
+    val itemText: TextView = itemView.findViewById(R.id.text)
 }
