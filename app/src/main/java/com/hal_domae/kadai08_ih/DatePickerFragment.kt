07a681dd77716d60c6e705d14abbfbfdf892136f
haldomae <hal.domae@gmail.com>
2024-12-12 16:33:19 +0900

    DatePickerFragmentを作成

---
+++ app/src/main/java/com/hal_domae/kadai08_ih/DatePickerFragment.kt
@@ -0,0 +1,35 @@
+package com.hal_domae.kadai08_ih
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.icu.util.Calendar
+import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.EditText
+import androidx.fragment.app.DialogFragment
+
+class DatePickerFragment: DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // 現在の日付を取得
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        return activity?.let {
+            DatePickerDialog(
+                it,
+                this,
+                year,
+                month,
+                day
+            )
+        }?: throw IllegalStateException("画面がありません")
+    }
+
+    // DatePickerで日付を選択し、ＯＫを押すと日付が反映される
+    override fun onDateSet(p0: DatePicker?, p1: Int, p2: Int, p3: Int) {
+        activity?.findViewById<EditText>(R.id.select_date)?.setText(getString(R.string.formatted_date,p1, p2, p3))
+    }
+}
