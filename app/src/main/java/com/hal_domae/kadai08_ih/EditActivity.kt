6368fb396cc80084bbceb95fb42c174353b6d0e0
haldomae <hal.domae@gmail.com>
2024-12-12 16:56:23 +0900

    気分を選択した時の処理を追加

--- app/src/main/java/com/hal_domae/kadai08_ih/EditActivity.kt
+++ app/src/main/java/com/hal_domae/kadai08_ih/EditActivity.kt
@@ -1,6 +1,7 @@
 package com.hal_domae.kadai08_ih
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +10,7 @@ import com.hal_domae.kadai08_ih.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditBinding
+    private var textFeeling: String? = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,5 +27,25 @@ class EditActivity : AppCompatActivity() {
             val datePicker = DatePickerFragment()
             datePicker.show(supportFragmentManager, "datePicker")
         }
+
+        // 気分は?を押した時のイベントリスナー
+        binding.feelGreat.setOnClickListener { feelClicked(it) }
+        binding.feelGood.setOnClickListener { feelClicked(it) }
+        binding.feelNormal.setOnClickListener { feelClicked(it) }
+        binding.feelBad.setOnClickListener { feelClicked(it) }
+        binding.feelAwful.setOnClickListener { feelClicked(it) }
+    }
+
+    // 気分が選択されたときの共通の関数
+    private fun feelClicked(view: View){
+        // 部品のIDで判別
+        textFeeling = when(view.id){
+            R.id.feel_great -> "今日の気分は最高でした!"
+            R.id.feel_good -> "今日の気分はいい!"
+            R.id.feel_normal -> "今日の気分は普通"
+            R.id.feel_bad -> "今日の気分は微妙..."
+            else -> "今日の気分は最悪..."
+        }
+        binding.inputDiary.setText(getString(R.string.diary_text, textFeeling, ""))
     }
 }
